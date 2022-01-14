c02c0463b82b9f5facac650dbf5e30ba351c9900
Alex Portas <alexalonsoportas@gmail.com>
2022-01-14 11:59:31 +0100

    añadir imagenes

--- juego.js
+++ juego.js
@@ -27,11 +27,11 @@ var game = new Phaser.Game(config);
 
 function preload ()
 {
-    this.load.image('sky', 'src/games/firstgame/assets/sky.png');
-    this.load.image('ground', 'src/games/firstgame/assets/platform.png');
-    this.load.image('star', 'src/games/firstgame/assets/star.png');
-    this.load.image('bomb', 'src/games/firstgame/assets/bomb.png');
-    this.load.spritesheet('dude', 'src/games/firstgame/assets/dude.png', { frameWidth: 32, frameHeight: 48 });
+    this.load.image('sky', 'img/sky.png');
+    this.load.image('ground', 'img/platform.png');
+    this.load.image('star', 'img/star.png');
+    this.load.image('bomb', 'img/bomb.png');
+    this.load.spritesheet('dude', 'img/dude.png', { frameWidth: 32, frameHeight: 48 });
 }
 
 function create ()
